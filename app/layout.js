752b0cd24598cf5b1cc3146b3907ac03b9a144e5
Vasile <allfortrashmail@gmail.com>
2022-12-01 18:25:46 +0200

    remove error boundary from layout app

--- app/layout.js
+++ app/layout.js
@@ -2,11 +2,9 @@
 
 import { Suspense } from "react";
 import { ChakraProvider, Box, Container } from "@chakra-ui/react";
-import { ErrorBoundary } from "react-error-boundary";
 import { AppHeader, AppFooter } from "components";
 import { theme, navigationHeight, footerHeight } from "util/theme-config";
 import Loading from "./loading";
-import Error from "./error";
 
 export default function RootLayout({ children }) {
 	return (
@@ -18,9 +16,7 @@ export default function RootLayout({ children }) {
 
 					<Container maxW="container.xl">
 						<Box as="main" minHeight={`calc(100vh - ${navigationHeight}px - ${footerHeight}px)`}>
-							<ErrorBoundary FallbackComponent={Error}>
-								<Suspense fallback={<Loading />}>{children}</Suspense>
-							</ErrorBoundary>
+							<Suspense fallback={<Loading />}>{children}</Suspense>
 						</Box>
 						<AppFooter />
 					</Container>
