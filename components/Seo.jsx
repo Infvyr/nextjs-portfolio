eec2de53c4b34608debeaac95221057d38db1ef4
Vasile <allfortrashmail@gmail.com>
2022-12-02 10:38:33 +0200

    edit seo

--- components/Seo.jsx
+++ components/Seo.jsx
@@ -1,34 +1,27 @@
-import { NextSeo } from "next-seo";
-
 export function Seo() {
 	const seo = {
+		author: "Vasile Novatchii",
 		title: "Vasile Novatchii",
 		description:
-			"A front-end developer from Moldova, who loves to develop beautiful websites, single page applications, customer relationship management or code from scratch. I create full UI from scratch or using UI libraries working with React or Next.js",
-		url: "https://vasile-novatchii.netlify.app"
+			"A front-end developer from Moldova, who loves to develop beautiful websites, single page applications, customer relationship management or code from scratch using React and Next.js",
+		url: "https://vasile-novatchii.netlify.app",
+		keywords:
+			"Portfolio website, React Portfolio, Next.js Portfolio, Frontend Developer Portfolio, Chakra Portfolio"
 	};
 
 	return (
-		<NextSeo
-			title={seo.title}
-			titleTemplate={seo.title}
-			defaultTitle={seo.title}
-			description={seo.description}
-			canonical={seo.url}
-			openGraph={{
-				type: "website",
-				url: seo.url,
-				title: seo.title,
-				description: seo.description,
-				images: [
-					{
-						url: "/screenshot.webp",
-						width: 800,
-						height: 288,
-						alt: "Vasile Novatchii"
-					}
-				]
-			}}
-		/>
+		<>
+			<link rel="canonical" href={seo.url} />
+			<meta name="description" content={seo.description} />
+			<meta name="keywords" content={seo.keywords} />
+			<meta name="author" content={seo.author} />
+			<meta name="og:title" content={seo.title} />
+			<meta property="og:site_name" data-page-subject="true" content={seo.title} />
+			<meta property="og:url" content={seo.url} />
+			<meta name="og:description" content={seo.description} />
+			<meta name="og:image" content={seo.url + "/screenshot.webp"} />
+			<meta property="og:image:width" content="800" />
+			<meta property="og:image:height" content="288" />
+		</>
 	);
 }
