321ec69d9cbfb4dd754a0b504b4b95fa96ba61f4
Infvyr <allfortrashmail@gmail.com>
2023-05-14 21:57:50 +0300

    style filter

--- app/projects/components/Filter.jsx
+++ app/projects/components/Filter.jsx
@@ -11,6 +11,7 @@ export function Filter({ onClick = (f) => f }) {
 		<LazyMotion features={domAnimation}>
 			<div
 				ref={animRef}
+				className="flex items-center gap-4 my-5"
 				style={{
 					opacity: isInView ? 1 : 0,
 					transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
@@ -19,17 +20,33 @@ export function Filter({ onClick = (f) => f }) {
 				<h3 aria-label="Filter projects" tabIndex="0">
 					Filter by:
 				</h3>
-				<div>
-					<button onClick={() => onClick(undefined)} aria-label="Show all projects">
+				<div className="flex items-center gap-4">
+					<button
+						className="icon-link-btn"
+						onClick={() => onClick(undefined)}
+						aria-label="Show all projects"
+					>
 						All
 					</button>
-					<button onClick={() => onClick("React")} aria-label="Filter projects by react">
+					<button
+						className="icon-link-btn"
+						onClick={() => onClick("React")}
+						aria-label="Filter projects by react"
+					>
 						<FaReact size="20" />
 					</button>
-					<button onClick={() => onClick("Next")} aria-label="Filter projects by next">
+					<button
+						className="icon-link-btn"
+						onClick={() => onClick("Next")}
+						aria-label="Filter projects by next"
+					>
 						<TbBrandNextjs size="20" />
 					</button>
-					<button onClick={() => onClick("Javascript")} aria-label="Filter projects by javascript">
+					<button
+						className="icon-link-btn"
+						onClick={() => onClick("Javascript")}
+						aria-label="Filter projects by javascript"
+					>
 						<TbBrandJavascript size="20" />
 					</button>
 				</div>
